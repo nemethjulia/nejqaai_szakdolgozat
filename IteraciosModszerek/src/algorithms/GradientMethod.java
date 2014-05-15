21780dd3254dd00dcc9684fea55cda3b055e0bc2
nemethjulia <nemethjulia0@gmail.com>
2014-05-15 09:59:10 +0200

    gradient bugfix + gitignore

--- IteraciosModszerek/src/algorithms/GradientMethod.java
+++ IteraciosModszerek/src/algorithms/GradientMethod.java
@@ -1,8 +1,5 @@
 package algorithms;
 
-import java.util.List;
-
-import datastructures.MatrixItem;
 import datastructures.MySparseMatrix;
 import datastructures.MySparseVector;
 
@@ -18,67 +15,23 @@ public class GradientMethod extends Method {
 
 	@Override
 	public MySparseVector iterationStep(MySparseVector x, MySparseMatrix m, MySparseVector b) {
-		int n = x.getSize();
-		MySparseVector xnew = new MySparseVector(n);
-		List<MatrixItem> data = m.getData();
-
 		// rk eloallitasa rk=b-A*x(k-1)
 
-		// segedvaltozok
-		int row = 0;
-		double rowsum = 0;
-		double value = 0;
-
-		MySparseVector rk = new MySparseVector(n);
-
-		for (MatrixItem i : data) {
-			if (i.getRow() != row) {
-				value = b.getValue(row) - rowsum;
-				rk.setValue(row, value);
-				row = i.getRow();
-				rowsum = 0;
-			} else
-				rowsum += i.getValue() * x.getValue(i.getCol());
-		}
-		value = b.getValue(row) - rowsum;
-		rk.setValue(row, value);
+		MySparseVector rk = b.substract(m.multiple(x));
 
 		// alpha kiszamitasa
 
 		// szamlalo
+		// r(k-1)T * r(k-1)
 
-		double szamlalo = 0;
-		value = 0;
-
-		for (int i = 0; i < n; ++i) {
-			value = rk.getValue(i);
-			szamlalo += value * value;
-		}
+		double szamlalo = rk.multiple(rk);
 
 		// nevezo
+		// r(k-1)T * A * r(k-1)
 
 		// kihasznaljuk, h a matrix szimmetrikus
 
-		row = 0;
-		rowsum = 0;
-
-		MySparseVector current = new MySparseVector(n);
-
-		for (MatrixItem i : data) {
-			if (i.getRow() != row) {
-				current.setValue(row, rowsum);
-				row = i.getRow();
-				rowsum = 0;
-			} else
-				rowsum += i.getValue() * rk.getValue(i.getCol());
-		}
-		current.setValue(row, rowsum);
-
-		double nevezo = 0;
-
-		for (int i = 0; i < n; ++i) {
-			nevezo += current.getValue(i) * rk.getValue(i);
-		}
+		double nevezo = rk.multiple(m.multiple(rk));
 
 		// es vegre az alpha
 
@@ -86,9 +39,7 @@ public class GradientMethod extends Method {
 
 		// xnew kiszmolasa : xk=x(k-1) + alpha * rk
 
-		for (int i = 0; i < n; ++i) {
-			xnew.setValue(i, x.getValue(i) + alpha * rk.getValue(i));
-		}
+		MySparseVector xnew = x.add(rk.multiple(alpha));
 
 		return xnew;
 	}
